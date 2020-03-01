8d37be06a48cba3c07b9ed0f6dc40487049a520e
adrian-vlad <ace_11_89@yahoo.com>
2020-03-02 00:51:38 +0200

    Added supervisor and ffmpeg to complete the realtime view setup

--- janus/scripts/config_prepare.py
+++ janus/scripts/config_prepare.py
@@ -6,18 +6,40 @@ import os
 import sys
 import yaml
 
-janus_port_streaming_start = int(os.environ["JANUS_PORT_STREAMING_START"])
-streaming_config_file_path = os.environ["STREAMING_CFG_PATH"]
-janus_cfg_streaming_file_path = os.path.join(os.environ["JANUS_CFG_DIR"], "janus.plugin.streaming.jcfg")
+def install_config_file_from_template(conf_file_path, vars_dict):
+    with open(conf_file_path + ".j2") as f:
+        template = Template(f.read())
+    with open(conf_file_path, "w") as f:
+        f.write(template.render(vars_dict))
 
+vars = {
+    "streaming_config_file_path": os.environ["STREAMING_CFG_PATH"],
+    "janus_bin_path": os.environ["JANUS_BIN_PATH"],
+    "janus_port_streaming_start": int(os.environ["JANUS_PORT_STREAMING_START"]),
+    "janus_cfg_streaming_file_path": os.path.join(os.environ["JANUS_CFG_DIR"], "janus.plugin.streaming.jcfg"),
+    "supervisor_cfg_dir_path": os.environ["SUPERVISOR_CFG_DIR_PATH"]
+}
+
+#TODO: error on duplicated keys
 # load the streaming configuration
-with open(streaming_config_file_path, "r") as f:
+with open(vars["streaming_config_file_path"], "r") as f:
     streaming_config = yaml.safe_load(f)
 
-with open(janus_cfg_streaming_file_path + ".j2") as f:
-    template = Template(f.read())
+    # add ID to each stream because it will be used to create port numbers
+    idx = 1
+    for key, value in streaming_config.items():
+        value["id"] = idx
+        idx += 1
 
-with open(janus_cfg_streaming_file_path, "w") as f:
-    f.write(template.render(streaming_config=streaming_config, janus_port_streaming_start=janus_port_streaming_start))
+    vars["streaming_config"] = streaming_config
+
+#TODO: janus.jcfg port
+# Janus plugins
+install_config_file_from_template(vars["janus_cfg_streaming_file_path"], vars)
+
+# Supervisor conf files
+install_config_file_from_template(os.path.join(vars["supervisor_cfg_dir_path"], "conf.d", "janus.conf"), vars)
+install_config_file_from_template(os.path.join(vars["supervisor_cfg_dir_path"], "conf.d", "ffmpeg.conf"), vars)
+
 
 sys.exit(0)
--- janus/scripts/start.sh
+++ janus/scripts/start.sh
@@ -5,5 +5,5 @@ set -e
 # prepare janus configs
 python3 ${SCRIPTS_DIR_PATH}/config_prepare.py
 
-# start janus
-exec ${JANUS_BIN_PATH}
+# start supervisor
+exec supervisord -c ${SUPERVISOR_CFG_DIR_PATH}/supervisord.conf
