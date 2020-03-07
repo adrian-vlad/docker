925891376009c5dfab1dca8e91f323efdb7fc776
adrian-vlad <ace_11_89@yahoo.com>
2020-03-07 22:36:31 +0200

    Added camera recording

--- janus/opt/scripts/config_prepare.py
+++ janus/opt/scripts/config_prepare.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import datetime
 from jinja2 import Template
+import json
 import os
 import sys
 import yaml
@@ -13,17 +15,20 @@ def install_config_file_from_template(conf_file_path, vars_dict):
 
 vars = {
     "streaming_config_file_path": os.environ["STREAMING_CFG_PATH"],
-    "janus_port_streaming_start": int(os.environ["JANUS_PORT_STREAMING_START"])
+    "janus_port_streaming_start": int(os.environ["JANUS_PORT_STREAMING_START"]),
+    "live555_port_streaming_start": int(os.environ["LIVE555_PORT_STREAMING_START"]),
+    "recordings_dir_path": os.environ["RECORDINGS_DIR_PATH"]
 }
 
 #TODO: error on duplicated keys
+#TODO: names should contain only alphanum and underscore
 # load the streaming configuration
 with open(vars["streaming_config_file_path"], "r") as f:
     streaming_config = yaml.safe_load(f)
 
     # add ID to each stream because it will be used to create port numbers
     idx = 1
-    for key, value in streaming_config.items():
+    for key, value in streaming_config["camera"].items():
         value["id"] = idx
         idx += 1
 
@@ -35,14 +40,26 @@ template_file_list = [
     "/etc/janus/janus.plugin.streaming.jcfg",
 
     # supervisor configuration files
-    "/etc/supervisor/conf.d/process_listener.conf",
+    "/etc/supervisor/conf.d/listener.conf",
     "/etc/supervisor/conf.d/janus.conf",
-    "/etc/supervisor/conf.d/ffmpeg_live_stream.conf",
-    "/etc/supervisor/conf.d/live_streaming.conf",
+    "/etc/supervisor/conf.d/camera_processor.conf",
+    "/etc/supervisor/conf.d/live_view_server.conf",
     "/etc/supervisor/conf.d/nginx.conf",
 ]
 
 for template in template_file_list:
     install_config_file_from_template(template, vars)
+
+# Save the streaming config on disk
+with open("/etc/streaming.json", "w") as f:
+    json.dump(vars["streaming_config"], f)
+
+# Prepare the recordings directory
+for stream_name, config in vars["streaming_config"].items():
+    for i in range(0, 7):
+        str_date = (datetime.date.today() + datetime.timedelta(days=i)).strftime('%Y_%m_%d')
+        dir_path = os.path.join(vars["recordings_dir_path"], stream_name, str_date)
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
 
 sys.exit(0)
--- janus/opt/scripts/supervisor/process_listener.py
+++ janus/opt/scripts/supervisor/process_listener.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
+import datetime
+import json
 import os
-import sys
+import subprocess
 from supervisor.childutils import listener
 from supervisor import childutils
-from subprocess import call
-import datetime
+import sys
 
 
 def write_stderr(s):
@@ -20,22 +21,67 @@ def info(s):
 def err(s):
     log("[ERR] " + s)
 
+
 def supervisor_restart(process_name):
-    info("supervisorctl starting " + process_name)
-    with open(os.devnull, "w") as f:
-        call(["supervisorctl", "start", process_name], stdout=2, stderr=2)
+    info("supervisorctl restarting " + process_name)
+    subprocess.call(["supervisorctl", "restart", process_name], stdout=2, stderr=2)
+
+
+def supervisor_group_restart(groupName):
+    supervisor_restart(groupName + ":")
+
+
+def rtsp_stream_is_alive(stream_uri):
+    openrtsp_path = "/opt/live555/openRTSP"
+    try:
+        openrtsp_cmd = openrtsp_path + " -v -d 1 " + stream_uri + " 2> /dev/null | wc -l"
+        #debug("running '" + openrtsp_cmd + "'")
+        out = subprocess.check_output(openrtsp_cmd, shell=True)
+        if int(out) == 0:
+            # stream does not work
+            return False
+    except Exception as e:
+        err(str(e))
+        return False
+
+    return True
+
 
 def process_state_fatal(payload):
     pheaders, pdata = childutils.eventdata(payload + "\n")
-    process_name = pheaders["processname"]
+    groupName = pheaders["groupname"]
 
-    err("fatal " + process_name)
+    # restart the whole group
+    err("fatal " + groupName)
+    supervisor_group_restart(groupName)
 
-    supervisor_restart(process_name)
+
+g_ticks = -1
+def process_tick5():
+    global g_ticks
+    g_ticks = (g_ticks + 1) % 2
+    if g_ticks != 0:
+        # do stuff only once every 10 seconds
+        return
+
+    # check if streams are alive
+    with open("/etc/streaming.json") as f:
+        streaming_config = json.load(f)
+
+    live555_port_start = int(os.environ["LIVE555_PORT_STREAMING_START"])
+
+    for name, config in streaming_config["camera"].items():
+        if not rtsp_stream_is_alive("rtsp://localhost:" + str(live555_port_start + config["id"]) + "/proxyStream"):
+            err("stream not live for " + name)
+            supervisor_group_restart(name)
+
 
 def process_event(msg_hdr, msg_payload):
     if msg_hdr["eventname"] == "PROCESS_STATE_FATAL":
         process_state_fatal(msg_payload)
+    if msg_hdr["eventname"] == "TICK_5":
+        process_tick5()
+
 
 def main():
     while True:
