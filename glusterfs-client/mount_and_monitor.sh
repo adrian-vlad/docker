c3f679c7c2329e3aa2d2c1aa1c1e97c989ed8c7a
adrian-vlad <ace_11_89@yahoo.com>
2020-11-08 20:34:48 +0200

    Try rshared

--- glusterfs-client/mount_and_monitor.sh
+++ glusterfs-client/mount_and_monitor.sh
@@ -7,7 +7,7 @@ mkdir -p "${TARGET_DIR}"
 while true
 do
   if ! mountpoint -q "${TARGET_DIR}"; then
-    mount -t glusterfs "${VOLUME_URL}" "${TARGET_DIR}"
+    mount --make-rshared -t glusterfs "${VOLUME_URL}" "${TARGET_DIR}"
   fi
 
   sleep 1
