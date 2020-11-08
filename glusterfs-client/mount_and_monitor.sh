1a9a5d6b480a10a77df2ac2e5f2dfb1d8ff0c06a
adrian-vlad <ace_11_89@yahoo.com>
2020-11-08 14:52:25 +0200

    Added a docker for gluster client

---
+++ glusterfs-client/mount_and_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bashs
+
+TARGET_DIR=/data/share
+
+mkdir -p "${TARGET_DIR}"
+
+while true
+do
+  if ! mountpoint -q "${TARGET_DIR}"; then
+    mount -t glusterfs "${VOLUME_URL}" "${TARGET_DIR}"
+  fi
+
+  sleep 1
+done
